29b45d66c663efe8852a49eaa96b886c7e8fbb02
Juan David Merchan Torres <juan1014166@gmail.com>
2022-05-30 20:02:32 -0500

    feat(ch3): Add basename1

---
+++ ch3/basename1/main.go
@@ -0,0 +1,21 @@
+// basename removes directory components and a .sufffix.
+// e.g., a => a, a.go => a, a/b/c.go => c, a/b.c.go => b.c
+package main
+
+func basename(s string) string {
+	// Discard last '/' and everything before.
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '/' {
+			s = s[i+1:]
+			break
+		}
+	}
+	// Preserve everythinh before last '.'.
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '.' {
+			s = s[:i]
+			break
+		}
+	}
+	return s
+}
